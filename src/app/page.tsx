676e72ba6473d144ede13f12217fe76743dd13b5
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-08-10 16:01:04 +0300

    feat: analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import { Services } from "@/Components/Services";
 import TireRequestForm from "@/Components/TiresForm";
 import { TuningCTA } from "@/Components/TuningCTA";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="mt-[70px] flex flex-col items-center justify-start">
+      <Analytics />
       <Toaster position="top-center" reverseOrder={false} />
       <Hero />
       <Services />
