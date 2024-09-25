fe7ff78fd7188b3cbec2977b3549451ff99a702d
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-09-25 19:13:32 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "автосервиз Misfire",
+    default: "Автосервиз Misfire",
     template: "%s - Misfire"
   },
   openGraph: {
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
       { url: '/misfire.png' }
     ]
   },
-  description: "пътна помощ, диагностика и общи ремонти, сервиз за гуми, тунинг и още много в aвтосервиз Misfire, град Пловдив",
+  description: "пътна помощ, диагностика ,диагностика и общи ремонти, сервиз за гуми, тунинг и още много в aвтосервиз Misfire, град Пловдив",
   keywords: ["автосервиз", "сервиз", "диагностика", "ремонт на кола", "пътна помощ", "тунинг", "тунинг пловдив", "пътна помощ пловдив", "сервиз пловдив", "смяна на гуми пловдив", "смяна на масло пловдив", "автосервиз пловдив"]
 };
 
@@ -26,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="bg">
       <body className={inter.className}>
-        <title>Misfire</title>
+        <title>Автосервиз Misfire</title>
         <link rel="icon" href="/misfire.png" />
         <Navbar />
         {children}
