d09a6f2944096678a8ed1647c4e4238a80e5b7ef
IvailoAtsv <ivailoatanassovv@gmail.com>
2024-10-08 13:37:18 +0300

    Update Contact.tsx

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -65,7 +65,7 @@ export const Contact = () => {
   };
 
   return (
-    <Animation maxW='3440px' moveY={150} opacity={1} start={0.7}>
+    <Animation maxW='3440px' moveY={150} opacity={1}>
         {sent ? '' : <h2 className='z-50 text-white text-h2clamp absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] animate-pulse'>Изпращане...</h2>}
       <div className={`${sent? 'opacity-100' : 'opacity-20' } relative p-6 max-w-[3440px] mx-auto w-full h-auto bg-accent flex justify-center items-center`}>
         <img
